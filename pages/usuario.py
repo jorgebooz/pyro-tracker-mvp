fe418cd5d6d509332033ae8092fda458db06aa3a
jorgebooz <jorg.jhfb@gmail.com>
2025-06-07 00:28:42 -0300

    update

--- pages/usuario.py
+++ pages/usuario.py
@@ -94,12 +94,11 @@ def render():
         lon_min, lon_max = -46.57, -46.48
 
         df_ativos = df[
-            ((df['id'] != 0) &
+            (df['id'] != 0) &
             (df['timestamp'] >= limite) &
             (df['latitude'] >= lat_min) & (df['latitude'] <= lat_max) &
-            (df['longitude'] >= lon_min) & (df['longitude'] <= lon_max))
+            (df['longitude'] >= lon_min) & (df['longitude'] <= lon_max)
         ]
-
 
         if not df_ativos.empty:
             st.subheader("🔥 Focos de Incêndio Ativos Próximos")
